a4252f583226f3dff213d5dc5afce113204e9d60
MarcosPaulo775 <marcos775paulo@hotmail.com>
2018-05-09 16:04:40 -0300

    Services Funcionando

--- src/app/components/alocacao/alocacao.component.ts
+++ src/app/components/alocacao/alocacao.component.ts
@@ -6,6 +6,8 @@ import { PessoaService } from '../../core/service/pessoas.service';
 
 import { PessoaTarefa } from '../../core/model/pessoa-tarefa';
 import { Tarefa } from '../../core/model/tarefa';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { Pessoa } from '../../core/model/pessoa';
 
 @Component({
   selector: 'app-alocacao',
@@ -14,18 +16,77 @@ import { Tarefa } from '../../core/model/tarefa';
 })
 export class AlocacaoComponent implements OnInit {
 
-  alocacao: PessoaTarefa[];
+  formularioAlocacao: FormGroup;
+
   tarefas: Tarefa[];
+  alocacao: PessoaTarefa[];
+  pessoas: Pessoa[];
+
+  alocacaoCriada: PessoaTarefa;
+
+  tarefaSelecionada: Tarefa;
+  alocacaoSelecionada: PessoaTarefa;
+  pessoaAdd: Pessoa;
+
+  aloc: PessoaTarefa;
+
+  GPS: number;
 
   constructor(
+    private formBuilder: FormBuilder,
     private alocacaoService: AlocacaoService,
     private tarefaService: TarefasService,
     private pessoaService: PessoaService
   ) { }
 
-
+  
   ngOnInit() {
+    this.alocacao = this.alocacaoService.pessoaTarefa;
     this.tarefas = this.tarefaService.tarefa;
+    this.pessoas = this.pessoaService.pessoa;
+    console.log(this.tarefas);
+    this.formularioAlocacao = this.formBuilder.group({
+      selectTarefa: [null],
+      selectPessoa: [null],
+    });
+    this.alocacao = this.alocacaoService.pessoaTarefa;
+  }
+  
+  OnSubmit(){
+    this.alocacaoCriada = new PessoaTarefa;
+    //
+    var x = this.procuraAlocacao(this.formularioAlocacao.get('selectTarefa').value);
+    this.pessoaAdd = new Pessoa;
+    this.alocacaoSelecionada = new PessoaTarefa;
+
+    this.pessoaAdd = this.formularioAlocacao.get('selectPessoa').value;
+    this.alocacaoService.pessoaTarefa[x].pessoa.push(this.pessoaAdd);
+
+    this.alocacaoSelecionada.pessoa.push(this.pessoaAdd);//Agora Vai!
+  
+    //
+    this.alocacaoCriada = this.alocacaoService.x(this.alocacaoCriada);
+    this.alocacaoCriada.id = this.alocacaoService.setId(this.alocacaoService.pessoaTarefa);
+    this.alocacaoCriada.pessoa.push(this.formularioAlocacao.get('selectPessoa').value);
+    this.alocacaoCriada.tarefa = this.formularioAlocacao.get('selectTarefa').value;
+
+    this.alocacaoService.pessoaTarefa.push(this.alocacaoCriada);
+
+    this.tarefaSelecionada = this.formularioAlocacao.get('selectTarefa').value;
+    console.log("Alocacao Selecionaddawdwada:");
+    console.log(this.tarefaSelecionada);
+    console.log("Alocacao Selecionada:");
+    console.log(this.tarefaSelecionada);
+  
+  }
+  
+  procuraAlocacao(x: Tarefa):number{
+    for(let i=0; i<this.alocacaoService.pessoaTarefa.length; i++){
+      if(x == this.alocacaoService.pessoaTarefa[i].tarefa)
+        this.GPS = i;
+    }
+    return this.GPS;
+    
   }
 
 }
--- src/app/core/service/alocacao.service.ts
+++ src/app/core/service/alocacao.service.ts
@@ -10,24 +10,56 @@ import { Pessoa } from '../model/pessoa';
 @Injectable()
 export class AlocacaoService {
     
+    constructor(private pessoaService: PessoaService){}
+    
     static pessoaTarefa: PessoaTarefa[];
-    tarefas: Tarefa[];
-    pessoas: Pessoa[];
+    alocacao: PessoaTarefa;
 
-    getAlocacao(): PessoaTarefa[]{
-        return [
-            {id: 1, tarefa: this.tarefas[0], pessoa: this.pessoas}
-        ]
+    static pessoas: Pessoa[];
+    pessoa: Pessoa;
+
+    pessoaTarefa = this.y(this.pessoaTarefa);
+    pessoas = this.pessoaService.getPessoas();
+    aux: number;
+
+    abc: Pessoa[];
+    xxx: Tarefa;
+    n: number;
+    tarefaSelecionada: Tarefa;
+    alocacaoSelecionada: PessoaTarefa;
+    
+
+    setId(x: PessoaTarefa[]):Number{
+        return x.length;
     }
 
-    constructor(
-        private tarefaService: TarefasService,
-        private pessoaService: PessoaService
-    ){
-        this.pessoas = this.pessoaService.pessoa;
-        this.tarefas = this.tarefaService.tarefa;
+    x(a: PessoaTarefa): PessoaTarefa{
+        a.pessoa = new Array<Pessoa>();
+        a.tarefa = new Tarefa;
+        return a;
     }
 
+    y(b: PessoaTarefa[]): PessoaTarefa[]{
+        b= new Array<PessoaTarefa>();
+        return b;
+    }
 
+    setAlocacao(tarefa: Tarefa){
+        //Sussa
+        this.aux = this.pessoaTarefa.length;
+
+        this.alocacao = new PessoaTarefa;
+        this.alocacao.tarefa = tarefa;
+        this.alocacao.pessoa = new Array<Pessoa>();
+
+        this.pessoaTarefa.push(this.alocacao);
+
+        this.pessoaTarefa[this.aux].id = this.setId(this.pessoaTarefa);
+
+        console.log("Objeto");
+        console.log(this.alocacao);
+        console.log("Vetor");
+        console.log(this.pessoaTarefa);
+    }
 
 }
